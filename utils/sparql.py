dd0be6098d30bf5f32a25ca2d3b5ef246b56d07c
Asad Shahin Moghadam <asad.shmoghadam@gmail.com>
2018-12-31 19:42:24 +0330

    Fix - SPARQL Endpoint (Change dataset name)

--- utils/sparql.py
+++ utils/sparql.py
@@ -2,7 +2,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 # general scheme for SPARQL queries
 
-sparql = SPARQLWrapper("http://localhost:3030/dstest4/sparql")
+sparql = SPARQLWrapper("http://localhost:3030/falconds/sparql")
 
 
 class sparqlQuery:
