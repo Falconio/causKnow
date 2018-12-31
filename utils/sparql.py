b7b6ac1c98eec16a384244a0f88c46052ec75899
Asad Shahin Moghadam <asad.shmoghadam@gmail.com>
2018-12-31 19:53:44 +0330

    SPARQL endpoint

--- utils/sparql.py
+++ utils/sparql.py
@@ -2,7 +2,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 # general scheme for SPARQL queries
 
-sparql = SPARQLWrapper("http://localhost:3030/falconds/sparql")
+sparql = SPARQLWrapper("http://fuseki.pomeio.com/falconds/sparql")
 
 
 class sparqlQuery:
