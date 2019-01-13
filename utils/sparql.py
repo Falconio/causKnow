f366abb814dcc98b20cd7a6e94b2b9c8a68d88e5
Asad Shahin Moghadam <asad.shmoghadam@gmail.com>
2019-01-14 01:02:21 +0330

    Change Dataset URL - New TDB2 [causknow] with Openllet reasoner works with OWL 2

--- utils/sparql.py
+++ utils/sparql.py
@@ -2,7 +2,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 # general scheme for SPARQL queries
 
-sparql = SPARQLWrapper("http://localhost:3030/falconds/sparql")
+sparql = SPARQLWrapper("http://localhost:3030/causknow/sparql")
 
 
 class sparqlQuery:
